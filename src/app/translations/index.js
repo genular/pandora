f72893b35b1bb60e99a9ba5b30749884a850816b
LogIN- <info@ivantomic.com>
2021-03-26 14:32:18 +0000

    [hotfix] add new translations

--- src/app/translations/index.js
+++ src/app/translations/index.js
@@ -11,12 +11,12 @@ import estore from "@/utils/storage/settings";
 import elementEnLocale from "element-ui/lib/locale/lang/en";
 import translations from "@/translations/files/translated/en.json";
 
-
 Vue.use(VueI18n);
+
 const i18n = new VueI18n({
 	locale: estore.get("main-language") || "en",
 	fallbackLocale: "en",
-	messages: { "en": { ...translations, ...elementEnLocale }}
+	messages: { en: { ...translations, ...elementEnLocale } },
 });
 
 export default i18n;
