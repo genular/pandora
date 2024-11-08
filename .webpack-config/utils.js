31ce7fd0520b5ba2051b11f8f7ec31d8ec79d0c9
LogIN- <info@ivantomic.com>
2024-11-08 19:49:10 +0000

    [hotfix]

--- .webpack-config/utils.js
+++ .webpack-config/utils.js
@@ -13,52 +13,63 @@ _.cwd = file => {
  * @return {[type]}            [description]
  */
 
-_.configureEnviroment = (environment, argv) => {
+_.configureEnvironment = (environment, argv) => {
 	// Copy configuration template and try to set initial env variables
 	const envTemplateExample = path.resolve(__dirname, `../config/env_${environment}.example.json`);
 	const envTemplateFinal = path.join(__dirname, `../config/env_${environment}.json`);
 
-	// If destination file doesn't exists copy data from template
-	// if (!fs.existsSync(envTemplateFinal)) {
+	// If destination file doesn't exist, copy data from template
 	if (argv) {
 		let envTemplate = require(envTemplateExample);
 		let updatedVars = 0;
 
-		if (typeof argv.isDemoServer !== "undefined") {
-			envTemplate.isDemoServer = (argv.isDemoServer == 'true');
-			updatedVars++;
-		}
-		if (typeof argv.server_frontend !== "undefined") {
-			envTemplate.server.frontend = argv.server_frontend;
-			updatedVars++;
-		}
-		if (typeof argv.server_backend !== "undefined") {
-			envTemplate.server.backend = argv.server_backend;
-			updatedVars++;
-		}
-		if (typeof argv.server_homepage !== "undefined") {
-			envTemplate.server.homepage = argv.server_homepage;
-			updatedVars++;
-		}
-		if (typeof argv.api_secret !== "undefined") {
-			envTemplate.api.secret = argv.api_secret;
-			updatedVars++;
-		}
-		if (typeof argv.api_chargebee_site_name !== "undefined") {
-			envTemplate.api.chargebee_site_name = argv.api_chargebee_site_name;
-			updatedVars++;
-		}
-		if (typeof argv.api_chargebee !== "undefined") {
-			envTemplate.api.chargebee = argv.api_chargebee;
-			updatedVars++;
-		}
+		// Function to get value from argv or process.env
+		const getValue = (key, defaultValue) => {
+			let value;
+			if (typeof argv[key] !== "undefined") {
+				value = argv[key];
+				console.log(`Using ${key} from argv: ${value}`);
+			} else if (typeof process.env[key.toUpperCase()] !== "undefined") {
+				value = process.env[key.toUpperCase()];
+				console.log(`Using ${key} from environment variable: ${value}`);
+			} else {
+				value = defaultValue;
+				console.log(`Using default value for ${key}: ${value}`);
+			}
+			return value;
+		};
+
+
+		// Update variables from argv or environment variables
+		envTemplate.isDemoServer = getValue("isDemoServer", envTemplate.isDemoServer) === 'true';
+		if (envTemplate.isDemoServer) updatedVars++;
+		
+		envTemplate.server.frontend = getValue("SERVER_FRONTEND_URL", envTemplate.server.frontend);
+		if (envTemplate.server.frontend) updatedVars++;
+
+		envTemplate.server.backend = getValue("SERVER_BACKEND_URL", envTemplate.server.backend);
+		if (envTemplate.server.backend) updatedVars++;
+
+		envTemplate.server.homepage = getValue("SERVER_HOMEPAGE_URL", envTemplate.server.homepage);
+		if (envTemplate.server.homepage) updatedVars++;
+
+		envTemplate.api.secret = getValue("api_secret", envTemplate.api.secret);
+		if (envTemplate.api.secret) updatedVars++;
+
+		envTemplate.api.chargebee_site_name = getValue("api_chargebee_site_name", envTemplate.api.chargebee_site_name);
+		if (envTemplate.api.chargebee_site_name) updatedVars++;
+
+		envTemplate.api.chargebee = getValue("api_chargebee", envTemplate.api.chargebee);
+		if (envTemplate.api.chargebee) updatedVars++;
+
+		// If any variables were updated, write to the final file
 		if (updatedVars > 0) {
-			envTemplate = JSON.stringify(envTemplate, null, 2);
-			fs.writeFileSync(envTemplateFinal, envTemplate);
+			const updatedEnvTemplate = JSON.stringify(envTemplate, null, 2);
+			fs.writeFileSync(envTemplateFinal, updatedEnvTemplate);
 		}
+
 		// Example of usage:
 		// yarn run start:web --server_frontend=xyc --server_backend=xyc --server_homepage=xyc --api_secret=xyc --api_chargebee_site_name=xyc --api_chargebee=xyc
 	}
-	// }
 	return envTemplateFinal;
 };
